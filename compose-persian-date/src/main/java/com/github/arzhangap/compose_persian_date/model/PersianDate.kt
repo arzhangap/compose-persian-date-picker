8b437673df179f4a2f2f296d48b352fbafa8af16
arjang_ap <arjang.ap@gmail.com>
2024-09-26 11:49:41 +0330

    delete module.

--- compose-persian-date/src/main/java/com/github/arzhangap/compose_persian_date/model/PersianDate.kt
+++
@@ -1,7 +0,0 @@
-package com.github.arzhangap.compose_persian_date.model
-
-data class PersianDate(
-    val year: Int,
-    val month: Int,
-    val day: Int
-)
--- compose-persian-date/src/main/java/com/github/arzhangap/compose_persian_date/model/PickerType.kt
+++
@@ -1,5 +0,0 @@
-package com.github.arzhangap.compose_persian_date.model
-
-enum class PickerType {
-    DAY, MONTH, YEAR
-}
